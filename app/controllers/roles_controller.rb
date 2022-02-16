440cd2c132035d444500dccef45f4340fc6e47b5
Bruno Peruchi <peruchib@gmail.com>
2022-02-15 23:03:49 -0300

    i18n on controllers

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -25,7 +25,7 @@ class RolesController < ApplicationController
 
     respond_to do |format|
       if @role.save
-        format.html { redirect_to role_url(@role), notice: "Role was successfully created." }
+        format.html { redirect_to role_url(@role), notice: t('notice.created') }
         format.json { render :show, status: :created, location: @role }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class RolesController < ApplicationController
   def update
     respond_to do |format|
       if @role.update(role_params)
-        format.html { redirect_to role_url(@role), notice: "Role was successfully updated." }
+        format.html { redirect_to role_url(@role), notice: t('notice.updated') }
         format.json { render :show, status: :ok, location: @role }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class RolesController < ApplicationController
     @role.destroy
 
     respond_to do |format|
-      format.html { redirect_to roles_url, notice: "Role was successfully destroyed." }
+      format.html { redirect_to roles_url, notice: t('notice.destroyed') }
       format.json { head :no_content }
     end
   end
